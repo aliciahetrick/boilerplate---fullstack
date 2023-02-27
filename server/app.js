93b840a21d7eeec32e8373cdb3615b304e1f32a4
aliciahetrick <aliciahetrick@gmail.com>
2023-02-26 20:57:59 -0500

    Add port to express app

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
+app.use(express.static(path.join(__dirname, '..', 'public')))
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+app.use(function (err, req, res, next) {
+  console.error(err)
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || 'Internal server error.')
+})
+
+module.exports = app
--- server/index.js
+++ server/index.js
@@ -1,22 +1,4 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
+const port = process.env.PORT || 3000
+const app = require('./app')
 
-app.use(express.static(path.join(__dirname, '..', 'public')))
-app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../index.html'))
-})
-
-app.use(function (err, req, res, next) {
-  console.error(err)
-  console.error(err.stack)
-  res.status(err.status || 500).send(err.message || 'Internal server error.')
-})
-
-module.exports = app
+app.listen(port, () => console.log(`listening on port ${port}`))
